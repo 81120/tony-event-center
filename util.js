96bebeb102f37e2d074ee891fd81b50d6e382aff
81120 <nicolas.leo.hu@gmail.com>
2017-11-11 16:33:19 +0800

    fix: remove test

--- util.js
+++ util.js
@@ -1,4 +1,4 @@
 exports.isValidEventName = eventName => eventName && (typeof eventName === 'string')
 exports.isValidFunction = handler => handler && (typeof handler === 'function')
-exports.isValidHandlerList = handlerList => handlerList && ([] instanceof Array)
+exports.isValidHandlerList = handlerList => handlerList && (handlerList instanceof Array)
 exports.isValidTimesToExecute = times => (typeof times === 'number' && times > 0) || (typeof times !== 'number')
